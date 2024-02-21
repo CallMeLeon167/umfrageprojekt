43cd4243928c24b509bc6437275a0c57f0fc0b14
Leon Schmidt <leon18058r@gmail.com>
2024-02-21 21:50:41 +0100

    Add registration and login routes to backend/index.php

--- backend/index.php
+++ backend/index.php
@@ -3,11 +3,24 @@ require_once 'app/admin/cml-load.php';
 
 use CML\Classes\Router;
 use CML\Classes\DB;
+use CML\Classes\Login;
 
 $db = new DB();
 $router = new Router();
+$user = new Login();
 
 $router->addRoute('GET', '/', function () use ($router) {
     $router->isApi();
     echo json_encode(["message" => "Hey Na!!!"]);
+});
+
+$router->addRoute('GET', '/register', function () use ($router, $user) {
+    $router->isApi();
+    $user->register("callmeleon", "kontakt@callmeleon.de", "TestPassword1234");
+});
+
+$router->addRoute('GET', '/login', function () use ($router, $user) {
+    $router->isApi();
+    $user->login("kontakt@callmeleon.de", "TestPassword1234"); //das geht
+    // $user->login("callmeleon", "TestPassword1234"); //das geht auch :)
 });

2210e23bbb57d502796bd3db232bcc63faf24588
Leon <leon18058r@gmail.com>
2024-02-25 14:19:38 +0100

    Update main.css and header.vue

--- frontend/src/layout/header.vue
+++ frontend/src/layout/header.vue
@@ -1,14 +1,68 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <RouterLink to="/">Home</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/survey/list">Umfragen</RouterLink>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <div class="header__left">
+      <img src="/src/assets/logo.svg" alt="logo" class="logo">
+      <h2>Umfrage Portal</h2>
+    </div>
+    <div class="header__right">
+      <nav>
+        <ul>
+          <li>
+            <RouterLink to="/">Home</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/survey/list">Umfragen</RouterLink>
+          </li>
+        </ul>
+      </nav>
+      <Avatar imageUrl="https://unchainedcrypto.com/wp-content/uploads/2023/07/pfp-nft.png" class="header__avatar"></Avatar>
+    </div>
+  </header>
 </template>
 
-<script setup lang="ts"></script>
+<style>
+  header{
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid var(--border);
+    padding: 5px 40px;
+  }
+
+  .header__left{
+    display: flex;
+    align-items: center;
+  }
+
+  .header__left img{
+    height: 40px;
+    width: auto;
+  }
+
+  .header__right ul{
+    padding: 0;
+    list-style: none;
+    display: flex;
+  }
+
+  .header__right{
+    display: flex;
+    align-items: center;
+  }
+
+  #app .header__avatar{
+    height: 40px;
+    width: 40px;
+    border: 1px solid black;
+  }
+
+  .header__right a{
+    color: black;
+    text-decoration: none;
+    padding: 0 15px;
+    transition: all 0.3s;
+  }
+
+  .header__right a:hover{
+    opacity: .7;
+  }
+</style>

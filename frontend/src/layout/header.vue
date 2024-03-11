bbbaf720de34ddbed617353e8742192453ed1aa3
Christopher <christopher.falkner@web.de>
2024-03-11 12:19:42 +0100

    feat: home logo clickable

--- frontend/src/layout/header.vue
+++ frontend/src/layout/header.vue
@@ -1,9 +1,9 @@
 <template>
   <header>
-    <div class="header__left">
-      <img src="/src/assets/logo.svg" alt="logo" class="logo">
+    <RouterLink to="/" class="header__left">
+      <img src="/src/assets/logo.svg" alt="logo" class="logo" />
       <h3>Umfrage Portal</h3>
-    </div>
+    </RouterLink>
     <div class="header__right">
       <nav>
         <ul>
@@ -15,54 +15,62 @@
           </li>
         </ul>
       </nav>
-      <Avatar imageUrl="https://unchainedcrypto.com/wp-content/uploads/2023/07/pfp-nft.png" class="header__avatar"></Avatar>
+      <Avatar
+        imageUrl="https://unchainedcrypto.com/wp-content/uploads/2023/07/pfp-nft.png"
+        class="header__avatar"
+      ></Avatar>
     </div>
   </header>
 </template>
 
 <style>
-  header{
-    display: flex;
-    justify-content: space-between;
-    border-bottom: 2px solid var(--border);
-    padding: 5px 40px;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid var(--border);
+  padding: 5px 40px;
+}
 
-  .header__left{
-    display: flex;
-    align-items: center;
-  }
+.header__left {
+  display: flex;
+  align-items: center;
+  /* no link styling */
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
 
-  .header__left img{
-    height: 40px;
-    width: auto;
-  }
+.header__left img {
+  height: 40px;
+  width: auto;
+}
 
-  .header__right ul{
-    padding: 0;
-    list-style: none;
-    display: flex;
-  }
+.header__right ul {
+  padding: 0;
+  list-style: none;
+  display: flex;
+}
 
-  .header__right{
-    display: flex;
-    align-items: center;
-  }
+.header__right {
+  display: flex;
+  align-items: center;
+}
 
-  #app .header__avatar{
-    height: 40px;
-    width: 40px;
-    border: 1px solid black;
-  }
+#app .header__avatar {
+  height: 40px;
+  width: 40px;
+  border: 1px solid black;
+}
 
-  .header__right a{
-    color: black;
-    text-decoration: none;
-    padding: 0 15px;
-    transition: all 0.3s;
-  }
+.header__right a {
+  color: black;
+  text-decoration: none;
+  padding: 0 15px;
+  transition: all 0.3s;
+}
 
-  .header__right a:hover{
-    opacity: .7;
-  }
+.header__right a:hover {
+  opacity: 0.7;
+}
 </style>

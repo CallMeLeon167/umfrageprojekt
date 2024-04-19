6e7099aa82ee964a4b47ac47c44283fa2bd2da29
Leon <leon18058r@gmail.com>
2024-04-19 10:15:05 +0200

    Add ORDER BY clause to SELECT_STATS.sql

--- backend/sql/SELECT_STATS.sql
+++ backend/sql/SELECT_STATS.sql
@@ -2,3 +2,4 @@ SELECT a.a_username, a.a_emailaddress, count(*) AS votes
 FROM Account AS a
 INNER JOIN SurveyParticipation AS sp ON sp.sp_accountID = a.id
 GROUP BY sp.sp_accountID
+ORDER BY votes DESC

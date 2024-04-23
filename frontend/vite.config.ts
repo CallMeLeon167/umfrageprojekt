d9673d188f2e1e9b50eb7aaedc83ec4c6b2ca570
Christopher Falkner <c.falkner@agiqon.de>
2024-04-23 16:25:28 +0200

    fix: HMR in container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
   server: {
     hmr: {
         host: 'localhost',
+    },
+    watch: {
+      // when running container in wsl2, polling is necessary
+      // regular file watchers do not work
+      usePolling: true,
+      interval: 1000,
     }
   },
   resolve: {

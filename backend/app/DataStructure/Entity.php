3750f97621e28996ff0cecc1e14fd1fd971a3759
Lars Dinkel <lars.dinkel2@freenet.de>
2024-07-04 09:33:56 +0200

    docs: add comments

--- backend/app/DataStructure/Entity.php
+++ backend/app/DataStructure/Entity.php
@@ -2,12 +2,35 @@
 
 namespace CML\DataStructure;
 
-abstract class Entity {
-
+/**
+ * Class Entity
+ *
+ * This abstract class offers the managing the value assignment for the DB pattern 
+ */
+abstract class Entity 
+{
+    /**
+     * get the required fields for the db.
+     *
+     * This method is implented in the extending classes.
+     *
+     * @return array The required fields of the regarding object.
+     */
     protected abstract function getRequiredFields(): array;
+    /**
+     * get the field mapping for the assignment.
+     *
+     * This method is implented in the extending classes.
+     *
+     * @return array The required fields of the regarding object.
+     */
     protected abstract function getFieldMappings(): array;
-
-
+    /**
+     * get the assigned object with values useing getFieldMappings().
+     *
+     * @param array the values to assign
+     * @return ?self The required fields of the regarding object.
+     */
     public function hydrateFromDBRow(array $row): ?self {
         if (!$this->validateFields($row)) {
             return null;
@@ -19,7 +42,12 @@ abstract class Entity {
         }
         return $this;
     }
-
+    /**
+     * checks array for completeness with getRequiredFields.
+     *
+     * @param array the existing fields 
+     * @return bool
+     */
     private function validateFields(array $row): bool {
         foreach ($this->getRequiredFields() as $field) {
             if (!array_key_exists($field, $row)) {

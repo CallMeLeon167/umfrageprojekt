a3752feca26b8fa2afd62fc1ba8909e269aa50c6
Christopher Falkner <christopher.falkner@web.de>
2024-07-04 17:12:32 +0200

    fix: hide admin navigation on children

--- frontend/src/page/admin/AdminIndex.vue
+++ frontend/src/page/admin/AdminIndex.vue
@@ -1,17 +1,21 @@
 <template>
-  <h3>Zeige Board von:</h3>
-  <button>
-    <RouterLink to="/admin/category">Kategorien</RouterLink>
-  </button>
-  <button>
-    <RouterLink to="/admin/survey">Umfragen</RouterLink>
-  </button>
+  <div v-if="route.name == 'admin'">
+    <h2>Admin</h2>
+    <h3>Zeige Board von:</h3>
+    <button>
+      <RouterLink to="/admin/category">Kategorien</RouterLink>
+    </button>
+    <button>
+      <RouterLink to="/admin/survey">Umfragen</RouterLink>
+    </button>
+  </div>
   <router-view>
   </router-view>
 </template>
 
 <script setup lang="ts">
-
+import { RouterLink, useRoute } from 'vue-router';
+const route = useRoute();
 </script>
 
 <style scoped>
